aed098de894e4073fe009e8bd11130906c25dc9e
Scott Graham <scott.graham@uber.com>
2020-10-17 16:29:38 -0700

    Add benchmarking to main

--- README.md
+++ README.md
@@ -11,12 +11,26 @@ To prepare your system to run this solution:
 	`export GOPATH=<insert-path-to-directory-where-you-will-save-go-projects>`
 4. This project should be saved in the GOPATH directory
 
+## Running the program
+There are two ways to benchmark the functions. 
+1. IDE via *_test.go files 
+2. Command line
+    1. build the project `go build`
+    2. run main `go run main.go`
+
+## Notes
+* For benchmarking, I arbitrarily select the maximum value for values in the generate array to be 1000 
+
 ## Analysis 
 Detailed analysis of benchmarking in [Google Doc](https://docs.google.com/document/d/1F-eGgUg6_c0UZuk0HssdgZLyPe9k1zeEeCdCs-tGaWA/edit?usp=sharing)
 
-## Future work
+## Future work / shortcomings 
 * Create TargetFinder interface
-* Generalize for different data types
-* Run benchmarking in the main function   
+* Generalize for different data types using reflection 
+* Run benchmarking in the main function
+* Modify benchTime for main function benchmarking calls 
+* Further Benchmark research to determine if: 
+    * Input & benchmark names can be changed interatively
+    * A single slice can be shared by two Benchmarking functions 
 
 
--- main.go
+++ main.go
@@ -2,12 +2,106 @@ package main
 
 import (
 	"fmt"
+	"target_finder/slicegenerator"
 	"target_finder/targetfinder"
+	"testing"
 )
 
+func benchmarkFindValueInSliceBinary(value int, slice []int, b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		targetfinder.FindValueInSliceBinary(value, slice)
+	}
+}
+
+func benchmarkFindValueInSliceLinear(value int, slice []int, b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		targetfinder.FindValueInSliceLinear(value, slice)
+	}
+}
+
+func BenchmarkFindValueInSliceBinary10(b *testing.B) {
+	value, slice := slicegenerator.GenerateRandomOrderedSlice(10, 1000)
+	benchmarkFindValueInSliceBinary(value, slice, b)
+}
+
+func BenchmarkFindValueInSliceBinary100(b *testing.B) {
+	value, slice := slicegenerator.GenerateRandomOrderedSlice(100, 1000)
+	benchmarkFindValueInSliceBinary(value, slice, b)
+}
+
+func BenchmarkFindValueInSliceBinary1000(b *testing.B) {
+	value, slice := slicegenerator.GenerateRandomOrderedSlice(1000, 1000)
+	benchmarkFindValueInSliceBinary(value, slice, b)
+}
+
+func BenchmarkFindValueInSliceBinary10000(b *testing.B) {
+	value, slice := slicegenerator.GenerateRandomOrderedSlice(10000, 1000)
+	benchmarkFindValueInSliceBinary(value, slice, b)
+}
+
+func BenchmarkFindValueInSliceBinary100000(b *testing.B) {
+	value, slice := slicegenerator.GenerateRandomOrderedSlice(100000, 1000)
+	benchmarkFindValueInSliceBinary(value, slice, b)
+}
+
+func BenchmarkFindValueInSliceBinary1000000(b *testing.B) {
+	value, slice := slicegenerator.GenerateRandomOrderedSlice(1000000, 1000)
+	benchmarkFindValueInSliceBinary(value, slice, b)
+}
+
+func BenchmarkFindValueInSliceBinary10000000(b *testing.B) {
+	value, slice := slicegenerator.GenerateRandomOrderedSlice(10000000, 1000)
+	benchmarkFindValueInSliceBinary(value, slice, b)
+}
+
+func BenchmarkFindValueInSliceLinear10(b *testing.B) {
+	value, slice := slicegenerator.GenerateRandomOrderedSlice(10, 1000)
+	benchmarkFindValueInSliceLinear(value, slice, b)
+}
+
+func BenchmarkFindValueInSliceLinear100(b *testing.B) {
+	value, slice := slicegenerator.GenerateRandomOrderedSlice(100, 1000)
+	benchmarkFindValueInSliceLinear(value, slice, b)
+}
+
+func BenchmarkFindValueInSliceLinear1000(b *testing.B) {
+	value, slice := slicegenerator.GenerateRandomOrderedSlice(1000, 1000)
+	benchmarkFindValueInSliceLinear(value, slice, b)
+}
+
+func BenchmarkFindValueInSliceLinear10000(b *testing.B) {
+	value, slice := slicegenerator.GenerateRandomOrderedSlice(10000, 1000)
+	benchmarkFindValueInSliceLinear(value, slice, b)
+}
+
+func BenchmarkFindValueInSliceLinear100000(b *testing.B) {
+	value, slice := slicegenerator.GenerateRandomOrderedSlice(100000, 1000)
+	benchmarkFindValueInSliceLinear(value, slice, b)
+}
+
+func BenchmarkFindValueInSliceLinear1000000(b *testing.B) {
+	value, slice := slicegenerator.GenerateRandomOrderedSlice(1000000, 1000)
+	benchmarkFindValueInSliceLinear(value, slice, b)
+}
+
+func BenchmarkFindValueInSliceLinear10000000(b *testing.B) {
+	value, slice := slicegenerator.GenerateRandomOrderedSlice(10000000, 1000)
+	benchmarkFindValueInSliceLinear(value, slice, b)
+}
+
 func main() {
-	slice1 := []int{1, 1, 2, 4, 5, 5, 7, 9}
-	value1 := 5
-	targetCount1 := targetfinder.FindValueInSliceLinear(value1, slice1)
-	fmt.Println("targetCount1: %d", targetCount1)
+	fmt.Println("BenchmarkFindValueInSliceBinary10: ", testing.Benchmark(BenchmarkFindValueInSliceBinary10))
+	fmt.Println("BenchmarkFindValueInSliceBinary100: ", testing.Benchmark(BenchmarkFindValueInSliceBinary100))
+	fmt.Println("BenchmarkFindValueInSliceBinary1000: ", testing.Benchmark(BenchmarkFindValueInSliceBinary1000))
+	fmt.Println("BenchmarkFindValueInSliceBinary10000: ", testing.Benchmark(BenchmarkFindValueInSliceBinary10000))
+	fmt.Println("BenchmarkFindValueInSliceBinary100000: ", testing.Benchmark(BenchmarkFindValueInSliceBinary100000))
+	fmt.Println("BenchmarkFindValueInSliceBinary1000000: ", testing.Benchmark(BenchmarkFindValueInSliceBinary1000000))
+	fmt.Println("BenchmarkFindValueInSliceBinary10000000: ", testing.Benchmark(BenchmarkFindValueInSliceBinary10000000))
+	fmt.Println("BenchmarkFindValueInSliceLinear10: ", testing.Benchmark(BenchmarkFindValueInSliceLinear10))
+	fmt.Println("BenchmarkFindValueInSliceLinear100: ", testing.Benchmark(BenchmarkFindValueInSliceLinear100))
+	fmt.Println("BenchmarkFindValueInSliceLinear1000: ", testing.Benchmark(BenchmarkFindValueInSliceLinear1000))
+	fmt.Println("BenchmarkFindValueInSliceLinear10000: ", testing.Benchmark(BenchmarkFindValueInSliceLinear10000))
+	fmt.Println("BenchmarkFindValueInSliceLinear100000: ", testing.Benchmark(BenchmarkFindValueInSliceLinear100000))
+	fmt.Println("BenchmarkFindValueInSliceLinear1000000: ", testing.Benchmark(BenchmarkFindValueInSliceLinear1000000))
+	fmt.Println("BenchmarkFindValueInSliceLinear10000000: ", testing.Benchmark(BenchmarkFindValueInSliceLinear10000000))
 }
